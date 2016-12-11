63925217e14267259c2d7d30a57f17b208b25b4e
Michael Stokley <michael@michaelstokley.com>
2016-12-10 21:31:10 -0800

    query_companies returns json from third party

--- app/app.py
+++ app/app.py
@@ -4,10 +4,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-API_STUB = 'http://api.nordglobal.net/api'
+API = 'https://api.idatalabs.com/v1'
 API_KEY = os.environ.get('IDATALABS_API_KEY', '')
 
 @app.route('/')
-@app.route('/form')
 def form():
     return render_template('form.html')
+
+@app.route('/companies/<product_slug>')
+def query_companies(product_slug):
+    """Proxy the iDatalabs api endpoint."""
+    url = '/'.join([
+        API,
+        'companies',
+        'aggregate',
+        '?product_slug={}&groupby=industry'.format(product_slug),
+    ])
+    response = requests.get(url, auth=('', API_KEY))
+    if response.status_code == 200:
+        out = response.text
+    else:
+        out = 'Unable to retrieve data from third party API.'
+    return out
