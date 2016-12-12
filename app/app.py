a9a412f914b9ec3bc4740f037fa6eadcb5608cdc
Michael Stokley <michael@michaelstokley.com>
2016-12-11 21:33:17 -0800

    working

--- app/app.py
+++ app/app.py
@@ -1,28 +1,65 @@
 import os
 import requests
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+from flask_restful import Api, Resource
+
+IDATALABS_API = 'https://api.idatalabs.com/v1'
+IDATALABS_API_KEY = os.environ.get('IDATALABS_API_KEY', '')
 
 app = Flask(__name__)
-
-API = 'https://api.idatalabs.com/v1'
-API_KEY = os.environ.get('IDATALABS_API_KEY', '')
+api = Api(app)
 
 @app.route('/')
 def form():
+    """Render the main page."""
     return render_template('form.html')
 
-@app.route('/companies/<product_slug>')
-def query_companies(product_slug):
-    """Proxy the iDatalabs api endpoint."""
-    url = '/'.join([
-        API,
-        'companies',
-        'aggregate',
-        '?product_slug={}&groupby=industry'.format(product_slug),
-    ])
-    response = requests.get(url, auth=('', API_KEY))
-    if response.status_code == 200:
-        out = response.text
-    else:
-        out = 'Unable to retrieve data from third party API.'
-    return out
+class QueryCompanies(Resource):
+    """Proxy the iDatalabs endpoint."""
+    def get(self, product_slug):
+        url = '/'.join([IDATALABS_API,
+                        'companies',
+                        'aggregate',
+                        '?product_slug={}&groupby=industry'.format(product_slug),])
+
+        # TODO put this back together
+        a = jsonify({"companies_count": 1989,
+                        "data": [
+                            {"count": 1126,
+                             "industry": "No data"},
+                            {"count": 110,
+                             "industry": "Retail"},
+                            {"count": 61,
+                             "industry": "Consumer Goods"},
+                            {"count": 52,
+                             "industry": "Health, Wellness and Fitness"}
+                        ]})
+        b = jsonify({"companies_count": 1989,
+                        "data": [
+                            {"count": 1126,
+                             "industry": "No data"},
+                            {"count": 61,
+                             "industry": "Consumer Goods"},
+                            {"count": 52,
+                             "industry": "Health, Wellness and Fitness"}
+                        ]})
+        c = jsonify({"companies_count": 1989,
+                        "data": [
+                            {"count": 1126,
+                             "industry": "No data"},
+                            {"count": 61,
+                             "industry": "Consumer Goods"},
+                        ]})
+        import random
+        return random.choice([a, b, c])
+
+
+        '''
+        response = requests.get(url, auth=('', IDATALABS_API_KEY))
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            return jsonify({'companies_count': 0})
+        '''
+
+api.add_resource(QueryCompanies, '/companies/<string:product_slug>')
--- app/static/script.js
+++ app/static/script.js
@@ -1,8 +1,64 @@
-$( function() {
-    $.getJSON( "static/products.json", function(data) {
-        var products = data.data;
-        $( "#products" ).autocomplete({
-            source: products.map(function(p) {return p.name;})
+var randomColorFactor = function () {
+    return Math.round(Math.random() * 255);
+};
+
+var randomColor = function (opacity) {
+    return 'rgba('
+        + randomColorFactor()
+        + ','
+        + randomColorFactor()
+        + ','
+        + randomColorFactor()
+        + ','
+        + (opacity || '.3')
+        + ')';
+};
+
+var ctx = "chart-area";
+window.chart = new Chart(ctx, {
+    type: "pie",
+    data:  {
+        datasets: [{data: [], backgroundColor: [], hoverBackgroundColor: []}],
+        labels: []
+    }
+});
+
+$(function () {
+    $.getJSON( "static/products.json", function (data) {
+        var productsObj = data.data.reduce(function (total, current) {
+            total[current.name] = current.name_slug;
+            return total;
+        }, {});  // dump array into object
+
+        var productEl = $("#product");
+
+        productEl.autocomplete({ source: Object.keys(productsObj) });
+
+        $("#viewCompanies").button().click(function (event) {
+            event.preventDefault();
+            $.get("/companies/" + productsObj[productEl.val()], function (data) {
+                if (data.companies_count === 0) {
+                    alert("Company data for product not found.");
+                } else {
+                    var colors = data.data.map(function () {
+                        return randomColor(0.7);
+                    });
+                    var industries = data.data.map(function (e) {
+                        return e["industry"];
+                    });
+                    var counts = data.data.map(function (e) {
+                        return e["count"];
+                    });
+
+                    // NOTE animation requires mutating one step at a time
+                    window.chart.data.labels = industries;
+                    window.chart.data.datasets[0].data = counts;
+                    window.chart.data.datasets[0].backgroundColor = colors;
+                    window.chart.data.datasets[0].hoverBackgroundColor = colors;
+
+                    window.chart.update();
+                }
+            });
         });
     });
-} ); // add autocomplete to search field
+});
