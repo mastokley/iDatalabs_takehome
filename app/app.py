cb0e261a6f9a5986458a1fac11841053b7512fd9
Michael Stokley <michael@michaelstokley.com>
2016-12-12 10:47:12 -0800

    hits live api, reduces categories to top N

--- app/app.py
+++ app/app.py
@@ -17,49 +17,16 @@ def form():
 class QueryCompanies(Resource):
     """Proxy the iDatalabs endpoint."""
     def get(self, product_slug):
-        url = '/'.join([IDATALABS_API,
-                        'companies',
-                        'aggregate',
-                        '?product_slug={}&groupby=industry'.format(product_slug),])
-
-        # TODO put this back together
-        a = jsonify({"companies_count": 1989,
-                        "data": [
-                            {"count": 1126,
-                             "industry": "No data"},
-                            {"count": 110,
-                             "industry": "Retail"},
-                            {"count": 61,
-                             "industry": "Consumer Goods"},
-                            {"count": 52,
-                             "industry": "Health, Wellness and Fitness"}
-                        ]})
-        b = jsonify({"companies_count": 1989,
-                        "data": [
-                            {"count": 1126,
-                             "industry": "No data"},
-                            {"count": 61,
-                             "industry": "Consumer Goods"},
-                            {"count": 52,
-                             "industry": "Health, Wellness and Fitness"}
-                        ]})
-        c = jsonify({"companies_count": 1989,
-                        "data": [
-                            {"count": 1126,
-                             "industry": "No data"},
-                            {"count": 61,
-                             "industry": "Consumer Goods"},
-                        ]})
-        import random
-        return random.choice([a, b, c])
-
-
-        '''
+        url = '/'.join([
+            IDATALABS_API,
+            'companies',
+            'aggregate',
+            '?product_slug={}&groupby=industry'.format(product_slug),
+        ])
         response = requests.get(url, auth=('', IDATALABS_API_KEY))
         if response.status_code == 200:
             return jsonify(response.json())
         else:
-            return jsonify({'companies_count': 0})
-        '''
+            return jsonify({'status': 'failure'})
 
 api.add_resource(QueryCompanies, '/companies/<string:product_slug>')
--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,5 @@
+var MAX_CATEGORY_COUNT = 20;
+
 var randomColorFactor = function () {
     return Math.round(Math.random() * 255);
 };
@@ -16,9 +18,14 @@ var randomColor = function (opacity) {
 
 var ctx = "chart-area";
 window.chart = new Chart(ctx, {
-    type: "pie",
+    type: "horizontalBar",
     data:  {
-        datasets: [{data: [], backgroundColor: [], hoverBackgroundColor: []}],
+        datasets: [{
+            label: "Top " + MAX_CATEGORY_COUNT + " Industries by Company Count",
+            data: [],
+            backgroundColor: [],
+            hoverBackgroundColor: []
+        }],
         labels: []
     }
 });
@@ -36,17 +43,43 @@ $(function () {
 
         $("#viewCompanies").button().click(function (event) {
             event.preventDefault();
+
+            /*
+             this third party API call (which is proxied) takes a long time
+             TODO app ought to display spinner while data is loading
+             there doesn't seem to be much opportunity for pre-loading
+             you could, however, cache results once loaded
+            */
+
             $.get("/companies/" + productsObj[productEl.val()], function (data) {
-                if (data.companies_count === 0) {
+                if (data.status !== "success") {
                     alert("Company data for product not found.");
                 } else {
-                    var colors = data.data.map(function () {
+                    var topCategories = data.data.slice(0, MAX_CATEGORY_COUNT);
+                    if (data.data.length > MAX_CATEGORY_COUNT) {
+                        var aggregatedCategory = data
+                                .data
+                                .slice(MAX_CATEGORY_COUNT)
+                                .map(function (x) {return x.count;})
+                                .reduce(function (x, y) {
+                                    return x + y;
+                                }); // can't seem to fold map into reduce...
+                        topCategories.push({
+                            "count": aggregatedCategory,
+                            "industry": data
+                                .data
+                                .slice(MAX_CATEGORY_COUNT)
+                                .length + " smaller industries"
+                        });
+                    } // aggregate remaining industries into one category
+
+                    var colors = topCategories.map(function () {
                         return randomColor(0.7);
                     });
-                    var industries = data.data.map(function (e) {
+                    var industries = topCategories.map(function (e) {
                         return e["industry"];
                     });
-                    var counts = data.data.map(function (e) {
+                    var counts = topCategories.map(function (e) {
                         return e["count"];
                     });
 
