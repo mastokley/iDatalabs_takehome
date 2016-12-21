a09a2d2569b5fc059c6025faac8d842359fea14a
Michael Stokley <michael@michaelstokley.com>
2016-12-20 16:32:06 -0800

    custom flask app to avoid delimiter collision with vue

--- app/app.py
+++ app/app.py
@@ -6,13 +6,27 @@ from flask_restful import Api, Resource
 IDATALABS_API = 'https://api.idatalabs.com/v1'
 IDATALABS_API_KEY = os.environ.get('IDATALABS_API_KEY', '')
 
-app = Flask(__name__)
+
+class AlternateJinjaDelimitersFlask(Flask):
+    """Avoid delimiter collision with vue.js"""
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='{%',
+        block_end_string='%}',
+        variable_start_string='[[',
+        variable_end_string=']]',
+        comment_start_string='{#',
+        comment_end_string='#}',
+    ))
+
+app = AlternateJinjaDelimitersFlask(__name__)
+
 api = Api(app)
 
 @app.route('/')
-def form():
+def index():
     """Render the main page."""
-    return render_template('form.html')
+    return render_template('index.html')
 
 class QueryCompanies(Resource):
     """Proxy the iDatalabs endpoint."""
